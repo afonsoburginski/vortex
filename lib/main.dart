a5e1c6d0b8c822371876d637fdd5dd85aada21c4
Kyuorhan <jhonnyKyuokay96@gmail.com>
2022-03-02 20:43:15 -0400

    Update/Vortex - Construindo a tela Splash/Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:vortex/pages/root_page.dart';
+import 'package:vortex/src/modules/splash/login_page.dart';
+import 'package:vortex/src/modules/splash/splash_page.dart';
+//import 'package:vortex/src/modules/splash/splash_page.dart';
+
+import 'shared/themes/app_colors.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RootPage(),
-  ));
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vortex',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const SplashPage(),
+    );
+  }
 }
--- lib/util/play_movie_page_json.dart
+++
@@ -1,4 +0,0 @@
-const String name = "Homem-Aranha.mp4";
-const String movieUrl =
-    "https://drive.google.com/drive/vortex/1aXDa4rjD-BHDuTpcCS7wwH4Qt_6XRguH/Homem-Aranha.mp4";
-/* const String YoutubePlayer = "https://www.youtube.com/watch?v=rzOvXvBNzMc"; */
