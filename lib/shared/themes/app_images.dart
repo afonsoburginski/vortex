faa451397c022f3ce6f2f88333643375285f3a77
Afonso Kevin Burginski <83406890+afonsoburginski@users.noreply.github.com>
2022-03-09 23:35:24 -0400

    Splash Page Animation

--- lib/shared/themes/app_images.dart
+++ lib/shared/themes/app_images.dart
@@ -1,8 +1,5 @@
 class AppImages {
-  static const logo = "assets/images/logo-full.png";
-  static const vortexLogoDegrade = "assets/images/vortex-logo-degrade.svg";
   static const vortexLogo = "assets/images/vortex-logo.svg";
-  static const logoTitle = "assets/images/logo-title.svg";
 
   static const apple = "assets/images/apple-original.svg";
   static const google = "assets/images/facebook-original.svg";
--- lib/src/modules/splash/splash_page.dart
+++ lib/src/modules/splash/splash_page.dart
@@ -1,12 +1,75 @@
 import 'package:flutter/material.dart';
+// ignore: unused_import
 import 'package:flutter_svg/flutter_svg.dart';
+// ignore: unused_import
 import 'package:vortex/shared/themes/app_colors.dart';
+// ignore: unused_import
 import 'package:vortex/shared/themes/app_images.dart';
+import 'package:video_player/video_player.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  // video controller
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.asset(
+      'assets/nike_animation.mp4',
+    )
+      ..initialize().then((_) {
+        setState(() {});
+      })
+      ..setVolume(0.0);
+
+    _playVideo();
+  }
+
+  void _playVideo() async {
+    // playing video
+    _controller.play();
+
+    //add delay till video is complite
+    await Future.delayed(const Duration(seconds: 4));
+
+    // navigating to home Page
+    Navigator.pushNamed(context, '/');
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(
+                  _controller,
+                ),
+              )
+            : Container(),
+      ),
+    );
+  }
+}
+
+
+  /* @override
   Widget build(BuildContext context) {
     //   final authController = AuthController();
     //   authController.currentUser(context);
@@ -20,7 +83,7 @@ class SplashPage extends StatelessWidget {
         ),
         child: Center(
           child: SvgPicture.asset(
-            AppImages.vortexLogoDegrade,
+            AppImages.vortexLogo,
             height: 90,
             width: 90,
           ),
@@ -28,4 +91,4 @@ class SplashPage extends StatelessWidget {
       ),
     );
   }
-}
+} */
